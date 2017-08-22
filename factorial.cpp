3e2c0d1adc49a62169a4585b5a3436c8ba29057a
aman935 <singh.amansingh935@gmail.com>
2017-08-22 16:48:43 +0530

    Factorial changed

--- factorial.cpp
+++ factorial.cpp
@@ -1,8 +1,13 @@
 #include "functions.h"
+#include<iostream>
 
 int factorial(int n){
-    if(n!=1){
-	return(n * factorial(n-1));
-    }
-    else return 1;
+  while(n<1){
+    std::cout<<"Please enter a positive number\n";
+    std::cin>>n;
+  }
+  if(n!=1){
+    return(n * factorial(n-1));
+  }
+  else return 1;
 }
